cd4d97ec4ffd080bae8e627c3ed3bd637c38b74e
Scott Sperling <scsper@gmail.com>
2014-11-08 09:25:40 -0800

    add error checking

--- approximate-sqrt.go
+++ approximate-sqrt.go
@@ -5,9 +5,21 @@ import (
     "math"
 )
 
-func Sqrt(x float64, deltaMax float64) float64 {
+type ErrNegativeSqrt struct {
+    NegativeValue float64
+}
+
+func (e *ErrNegativeSqrt) Error() string {
+    return fmt.Sprintf("cannot Sqrt negative number: %f", e.NegativeValue)
+}
+
+func Sqrt(x float64, deltaMax float64) (float64, error) {
     var z, oldZ, delta float64 = 10.0, 0.0, 10.0
     count := 0
+
+    if x < 0 {
+        return -1, &ErrNegativeSqrt {x}
+    }
 
     for delta > deltaMax {
         oldZ = z
@@ -17,7 +29,7 @@ func Sqrt(x float64, deltaMax float64) float64 {
     }
 
     fmt.Println(count)
-    return z
+    return z, nil
 }
 
 func main() {
@@ -26,4 +38,7 @@ func main() {
         fmt.Println(Sqrt(float64(i), .01))
         fmt.Println(math.Sqrt(float64(i)))
     }
+
+    fmt.Println("there should be an error")
+    fmt.Println(Sqrt(-2, .01))
 }
