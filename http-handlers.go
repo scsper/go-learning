3e9bf2f78c152687ef4f4db9a79a4f3414e7a047
Scott Sperling <scsper@gmail.com>
2014-11-09 18:20:07 -0800

    more http handlers

--- http-handlers.go
+++ http-handlers.go
@@ -6,15 +6,26 @@ import (
 )
 
 type String string
+type Struct struct {
+    Greeting string
+    Punct string
+    Who String
+}
 
 func (s String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, s)
 }
 
+func (s Struct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, s.Greeting)
+    fmt.Fprint(w, s.Punct)
+    fmt.Fprint(w, s.Who)
+}
 
 func main() {
     // your http.Handle calls here
     http.Handle("/string", String("I'm a frayed knot."))
+    http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
 
     http.ListenAndServe("localhost:4000", nil)
 }
