5303f8a40c013ce62b0dad71b69acaf17f8e73be
Scott Sperling <scsper@gmail.com>
2014-11-12 21:01:57 -0800

    load a page

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -4,3 +4,19 @@ import (
     "fmt"
     "io/ioutil"
 )
+
+type Page struct {
+    Title string
+    Body []byte
+}
+
+func (p *Page) save() error {
+    filename := p.title + ".txt"
+    return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) *Page {
+    filename := title + ".txt"
+    body, _ := ioutil.ReadFile(filename)
+    return &Page{Title: title, Body: body}
+}
